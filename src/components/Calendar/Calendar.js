bf0941ff37cefab32085c749fec0c385743353f2
Son Kangwon <fjqlddb06@snu.ac.kr>
2020-10-09 06:30:26 +0900

    lab5

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -21,7 +21,7 @@ const renderCalenderBody = (dates, todos, clickDone) => {
   let i = 0;
   const rows = [];
   for (let week=0; week<5; week++){
-    let day = 0; // Sunday
+    // let day = 0; // Sunday
 
     let row = [];
     for (let day=0; day<7; day++) {
@@ -64,7 +64,7 @@ const renderCalenderBody = (dates, todos, clickDone) => {
 }
 
 const renderCalendar = (dates, todos, clickDone) => (
-  <Table striped style={{"height": "600px", "width": "600px"}}>
+  <Table className='Calendar' striped style={{"height": "600px", "width": "600px"}}>
     {CALENDAR_HEADER}
     {renderCalenderBody(dates, todos, clickDone)}
   </Table>
@@ -73,8 +73,8 @@ const renderCalendar = (dates, todos, clickDone) => (
 const Calendar = (props) => {
   const dates = [];
   const year = props.year;
-  const month = props.month - 1;
-  let date = 1;
+  const month = props.month-1;
+  
   let maxDate = (new Date(year, month + 1, 0)).getDate();
 
   for (let date=1; date<=maxDate; date++) {
--- src/containers/TodoList/NewTodo/NewTodo.test.js
+++ src/containers/TodoList/NewTodo/NewTodo.test.js
@@ -35,6 +35,10 @@ describe('<NewTodo />', () => {
     );
   })
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
   it('should render NewTodo', () => {
     const component = mount(newTodo);
     const wrapper = component.find('.NewTodo');
@@ -53,7 +57,7 @@ describe('<NewTodo />', () => {
   it(`should set state properly on title input`, () => {
     const title = 'TEST_TITLE'
     const component = mount(newTodo);
-    const wrapper = component.find('input');
+    const wrapper = component.find('input').at(0);
     wrapper.simulate('change', { target: { value: title } });
     const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
     expect(newTodoInstance.state.title).toEqual(title);
@@ -68,6 +72,39 @@ describe('<NewTodo />', () => {
     const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
     expect(newTodoInstance.state.title).toEqual('');
     expect(newTodoInstance.state.content).toEqual(content);
+  });
+
+  it(`should set state properly on year input`, () => {
+    const year = '2020'
+    const component = mount(newTodo);
+    const wrapper = component.find('#year');
+    wrapper.simulate('change', { target: { value: year } });
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.title).toEqual('');
+    expect(newTodoInstance.state.content).toEqual('');
+    expect(newTodoInstance.state.dueDate.year).toEqual(year);
+  });
+  
+  it(`should set state properly on content input`, () => {
+    const month = '10'
+    const component = mount(newTodo);
+    const wrapper = component.find('#month');
+    wrapper.simulate('change', { target: { value: month } });
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.title).toEqual('');
+    expect(newTodoInstance.state.content).toEqual('');
+    expect(newTodoInstance.state.dueDate.month).toEqual(month);
+  });
+
+  it(`should set state properly on content input`, () => {
+    const date = '9'
+    const component = mount(newTodo);
+    const wrapper = component.find('#date');
+    wrapper.simulate('change', { target: { value: date } });
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.title).toEqual('');
+    expect(newTodoInstance.state.content).toEqual('');
+    expect(newTodoInstance.state.dueDate.date).toEqual(date);
   });
 });
 
