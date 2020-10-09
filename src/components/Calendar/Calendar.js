4319a255766756178ee23ef1185ed23206f6a90b
hhjjpp <hyejung8053@gmail.com>
2020-10-09 22:13:19 +0900

    lab

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -64,7 +64,7 @@ const renderCalenderBody = (dates, todos, clickDone) => {
 }
 
 const renderCalendar = (dates, todos, clickDone) => (
-  <Table striped style={{"height": "600px", "width": "600px"}}>
+  <Table striped style={{height: "600px", width: "600px"}}>
     {CALENDAR_HEADER}
     {renderCalenderBody(dates, todos, clickDone)}
   </Table>
@@ -75,7 +75,7 @@ const Calendar = (props) => {
   const year = props.year;
   const month = props.month - 1;
   let date = 1;
-  let maxDate = (new Date(year, month + 1, 0)).getDate();
+  let maxDate = new Date(year, month + 1, 0).getDate();
 
   for (let date=1; date<=maxDate; date++) {
     dates.push(new Date(year, month, date));
