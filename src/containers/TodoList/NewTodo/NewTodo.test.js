13b846b92726d4e2bd6752413eeaa39e7edf97d9
shapespeare <shapespeare@snu.ac.kr>
2020-10-09 20:01:38 +0900

    Finished Lab

--- src/containers/TodoList/NewTodo/NewTodo.test.js
+++ src/containers/TodoList/NewTodo/NewTodo.test.js
@@ -53,8 +53,7 @@ describe('<NewTodo />', () => {
   it(`should set state properly on title input`, () => {
     const title = 'TEST_TITLE'
     const component = mount(newTodo);
-    const wrapper = component.find('input');
-    wrapper.simulate('change', { target: { value: title } });
+    const wrapper = component.find('#title');    wrapper.simulate('change', { target: { value: title } });
     const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
     expect(newTodoInstance.state.title).toEqual(title);
     expect(newTodoInstance.state.content).toEqual('');
@@ -69,6 +68,33 @@ describe('<NewTodo />', () => {
     expect(newTodoInstance.state.title).toEqual('');
     expect(newTodoInstance.state.content).toEqual(content);
   });
+
+  it(`should set state properly by year`, () => {
+    const year = 'TEST_YEAR';
+    const component = mount(newTodo);
+    const wrapper = component.find('#year');
+    wrapper.simulate('change', {target: {value: year}});
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.dueDate.year).toEqual(year);
+  });
+
+  it(`should set state properly by month`, () => {
+    const month = 'TEST_MONTH';
+    const component = mount(newTodo);
+    const wrapper = component.find('#month');
+    wrapper.simulate('change', {target: {value: month}});
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.dueDate.month).toEqual(month);
+  });
+
+  it(`should set state properly by date`, () => {
+    const date = 'TEST_DATE';
+    const component = mount(newTodo);
+    const wrapper = component.find('#date');
+    wrapper.simulate('change', {target: {value: date}});
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.dueDate.date).toEqual(date);
+  })
 });
 
 
